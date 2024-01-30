41139f65d70b1ade04dbd7b3ad543c96c4f83033
John Skinner <john@sknnr.io>
2024-01-29 23:18:24 -0600

    working on a proton release, still in progress

---
+++ container/entrypoint-proton.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# Validate arguments
+if [ -z "$SERVER_NAME" ]; then
+    SERVER_NAME='Enshrouded Containerized'
+    echo "WARN: SERVER_NAME not set, using default: Enshrouded Containerized"
+fi
+
+if [ -z "$SERVER_PASSWORD" ]; then
+    echo "ERROR: SERVER_PASSWORD not set, exitting"
+    exit 1
+fi
+
+if [ -z "$GAME_PORT" ]; then
+    GAME_PORT='15636'
+    echo "WARN: GAME_PORT not set, using default: 15636"
+fi
+
+if [ -z "$QUERY_PORT" ]; then
+    QUERY_PORT='15637'
+    echo "WARN: QUERY_PORT not set, using default: 15637"
+fi
+
+if [ -z "$SERVER_SLOTS" ]; then
+    SERVER_SLOTS='16'
+    echo "WARN: SERVER_SLOTS not set, using default: 16"
+fi
+
+if [ -z "$SERVER_IP" ]; then
+    SERVER_IP='0.0.0.0'
+    echo "WARN: SERVER_IP not set, using default: 0.0.0.0"
+fi
+
+# Install/Update Enshrouded
+echo "INFO: Updating Enshrouded Dedicated Server"
+${STEAM_PATH}/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir "$ENSHROUDED_PATH" +login anonymous +app_update ${STEAM_APP_ID} validate +quit
+
+# Check that steamcmd was successful
+if [ $? != 0 ]; then
+    echo "ERROR: steamcmd was unable to successfully initialize and update Enshrouded..."
+    exit 1
+fi
+
+# Copy example server config if not already present
+if ! [ -f "${ENSHROUDED_PATH}/enshrouded_server.json" ]; then
+    echo "INFO: Enshrouded server config not present, copying example"
+    cp /home/steam/enshrouded_server_example.json ${ENSHROUDED_PATH}/enshrouded_server.json
+fi
+
+# Check for proper save permissions
+if ! touch "${ENSHROUDED_PATH}/savegame/test"; then
+    echo ""
+    echo "ERROR: The ownership of /home/steam/enshrouded/savegame is not correct and the server will not be able to save..."
+    echo "the directory that you are mounting into the container needs to be owned by 10000:10000"
+    echo "from your container host attempt the following command 'chown -R 10000:10000 /your/enshrouded/folder'"
+    echo ""
+    exit 1
+fi
+
+rm "${ENSHROUDED_PATH}/savegame/test"
+
+# Modify server config to match our arguments
+echo "INFO: Updating Enshrouded Server configuration"
+tmpfile=$(mktemp)
+jq --arg n "$SERVER_NAME" '.name = $n' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
+jq --arg p "$SERVER_PASSWORD" '.password = $p' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
+jq --arg g "$GAME_PORT" '.gamePort = ($g | tonumber)' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
+jq --arg q "$QUERY_PORT" '.queryPort = ($q | tonumber)' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
+jq --arg s "$SERVER_SLOTS" '.slotCount = ($s | tonumber)' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
+jq --arg i "$SERVER_IP" '.ip = $i' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
+
+# Wine talks too much and it's annoying
+export WINEDEBUG=-all
+
+# Need to copy the sharedobject or Enshrouded can't use the steam sdk
+ln -s ${STEAM_PATH}/steamcmd/linux64 /home/steam/.steam/sdk64
+ln -s ${STEAM_PATH}/steamcmd/linux32 /home/steam/.steam/sdk32
+ln -s ${STEAM_PATH}/steamcmd/linux64/steamclient.so /home/steam/.steam/sdk64/steamclient.so
+ln -s ${STEAM_PATH}/steamcmd/linux32/steamclient.so /home/steam/.steam/sdk32/steamclient.so
+ln -s /home/steam/.steam/sdk32/steamclient.so /home/steam/.steam/sdk32/steamservice.so
+ln -s /home/steam/.steam/sdk64/steamclient.so /home/steam/.steam/sdk64/steamservice.so
+
+
+# Check that log directory exists, if not create
+if ! [ -d "${ENSHROUDED_PATH}/logs" ]; then
+    mkdir -p "${ENSHROUDED_PATH}/logs"
+fi
+
+# Check that log file exists, if not create
+if ! [ -f "${ENSHROUDED_PATH}/logs/enshrouded_server.log" ]; then
+    touch "${ENSHROUDED_PATH}/logs/enshrouded_server.log"
+fi
+
+# Link logfile to stdout of pid 1 so we can see logs
+ln -sf /proc/1/fd/1 "${ENSHROUDED_PATH}/logs/enshrouded_server.log"
+
+# Launch Enshrouded
+echo "INFO: Starting Enshrouded Dedicated Server"
+${STEAM_PATH}/compatibilitytools.d/GE-Proton${GE_PROTON_VERSION}/proton run ${ENSHROUDED_PATH}/enshrouded_server.exe
+
+podman run -d --name enshrouded \
+  --hostname enshrouded \
+  --restart=unless-stopped \
+  -p 15636-15637:15636-15637/udp \
+  -v ./game:/opt/enshrouded \
+  -e SERVER_NAME="Enshrouded Server" \
+  -e SERVER_PASSWORD="secret" \
+  -e UPDATE_CRON="*/30 * * * *" \
+  -e PUID=4711 \
+  -e PGID=4711 \
+  docker.io/mornedhels/enshrouded-server:latest
--- makefile
+++ makefile
@@ -1,20 +1,25 @@
 # Image values
-REGISTRY := "localhost"
-PROJECT := "sknnr"
-IMAGE := "enshrouded-server-testing"
-IMAGE_REF := $(REGISTRY)/$(PROJECT)/$(IMAGE)
+REGISTRY := localhost
+IMAGE := enshrouded-test
+PROTON_IMAGE := enshrouded-proton-test
+IMAGE_REF := $(REGISTRY)/$(IMAGE)
+PROTON_IMAGE_REF := $(REGISTRY)/$(PROTON_IMAGE)
 
 # Git commit hash
 HASH := $(shell git rev-parse --short HEAD)
 
 # Buildah/Podman Options
-CONTAINER_NAME := "enshrouded-test"
-VOLUME_NAME := "persistent-data-test"
+CONTAINER_NAME := enshrouded-test
+VOLUME_NAME := enshrouded-data
 BUILDAH_BUILD_OPTS := --format docker -f ./container/Containerfile
 PODMAN_RUN_OPTS := --name $(CONTAINER_NAME) -d --mount type=volume,source=$(VOLUME_NAME),target=/home/steam/enshrouded/savegame -p 15636:15636/udp -p 15637:15637/udp --env=SERVER_NAME='Enshrouded Containerized Server' --env=SERVER_SLOTS=16 --env=SERVER_PASSWORD='ChangeThisPlease' --env=GAME_PORT=15636 --env=QUERY_PORT=15637
+PROTON_CONTAINER_NAME := enshrouded-proton-test
+PROTON_VOLUME_NAME := enshrouded-proton-data
+PROTON_BUILDAH_BUILD_OPTS := --format docker -f ./container/Containerfile-proton
+PROTON_PODMAN_RUN_OPTS := --name $(PROTON_CONTAINER_NAME) -d --mount type=volume,source=$(PROTON_VOLUME_NAME),target=/home/steam/enshrouded/savegame -p 15636:15636/udp -p 15637:15637/udp --env=SERVER_NAME='Enshrouded Containerized Server' --env=SERVER_SLOTS=16 --env=SERVER_PASSWORD='ChangeThisPlease' --env=GAME_PORT=15636 --env=QUERY_PORT=15637
 
 # Makefile targets
-.PHONY: build run cleanup
+.PHONY: build run cleanup build-proton run-proton cleanup-proton
 
 build:
 	buildah build $(BUILDAH_BUILD_OPTS) -t $(IMAGE_REF):$(HASH) ./container
@@ -27,3 +32,15 @@ cleanup:
 	podman rm -f $(CONTAINER_NAME)
 	podman rmi -f $(IMAGE_REF):$(HASH)
 	podman volume rm $(VOLUME_NAME)
+
+build-proton:
+	buildah build $(PROTON_BUILDAH_BUILD_OPTS) -t $(PROTON_IMAGE_REF):$(HASH) ./container
+
+run-proton:
+	podman volume create $(PROTON_VOLUME_NAME)
+	podman run $(PROTON_PODMAN_RUN_OPTS) $(PROTON_IMAGE_REF):$(HASH)
+
+cleanup-proton:
+	podman rm -f $(PROTON_CONTAINER_NAME)
+	podman rmi -f $(PROTON_IMAGE_REF):$(HASH)
+	podman volume rm $(PROTON_VOLUME_NAME)
