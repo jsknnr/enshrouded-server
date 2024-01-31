b078060632deb7425a4d68fec02895adf89da7fc
John Skinner <john@sknnr.io>
2024-01-31 02:16:21 -0600

    work for a proton image

--- container/proton/entrypoint.sh
+++ container/proton/entrypoint.sh
@@ -1,56 +1,61 @@
 #!/bin/bash
 
+# Quick function to generate a timestamp
+timestamp () {
+  date +"%Y-%m-%d %H:%M:%S,%3N"
+}
+
 # Validate arguments
 if [ -z "$SERVER_NAME" ]; then
     SERVER_NAME='Enshrouded Containerized'
-    echo "WARN: SERVER_NAME not set, using default: Enshrouded Containerized"
+    echo "$(timestamp) WARN: SERVER_NAME not set, using default: Enshrouded Containerized"
 fi
 
 if [ -z "$SERVER_PASSWORD" ]; then
-    echo "ERROR: SERVER_PASSWORD not set, exitting"
+    echo "$(timestamp) ERROR: SERVER_PASSWORD not set, exitting"
     exit 1
 fi
 
 if [ -z "$GAME_PORT" ]; then
     GAME_PORT='15636'
-    echo "WARN: GAME_PORT not set, using default: 15636"
+    echo "$(timestamp) WARN: GAME_PORT not set, using default: 15636"
 fi
 
 if [ -z "$QUERY_PORT" ]; then
     QUERY_PORT='15637'
-    echo "WARN: QUERY_PORT not set, using default: 15637"
+    echo "$(timestamp) WARN: QUERY_PORT not set, using default: 15637"
 fi
 
 if [ -z "$SERVER_SLOTS" ]; then
     SERVER_SLOTS='16'
-    echo "WARN: SERVER_SLOTS not set, using default: 16"
+    echo "$(timestamp) WARN: SERVER_SLOTS not set, using default: 16"
 fi
 
 if [ -z "$SERVER_IP" ]; then
     SERVER_IP='0.0.0.0'
-    echo "WARN: SERVER_IP not set, using default: 0.0.0.0"
+    echo "$(timestamp) WARN: SERVER_IP not set, using default: 0.0.0.0"
 fi
 
 # Install/Update Enshrouded
-echo "INFO: Updating Enshrouded Dedicated Server"
+echo "$(timestamp) INFO: Updating Enshrouded Dedicated Server"
 steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir "$ENSHROUDED_PATH" +login anonymous +app_update ${STEAM_APP_ID} validate +quit
 
 # Check that steamcmd was successful
 if [ $? != 0 ]; then
-    echo "ERROR: steamcmd was unable to successfully initialize and update Enshrouded..."
+    echo "$(timestamp) ERROR: steamcmd was unable to successfully initialize and update Enshrouded"
     exit 1
 fi
 
 # Copy example server config if not already present
 if ! [ -f "${ENSHROUDED_PATH}/enshrouded_server.json" ]; then
-    echo "INFO: Enshrouded server config not present, copying example"
+    echo "$(timestamp) INFO: Enshrouded server config not present, copying example"
     cp /home/steam/enshrouded_server_example.json ${ENSHROUDED_PATH}/enshrouded_server.json
 fi
 
 # Check for proper save permissions
 if ! touch "${ENSHROUDED_PATH}/savegame/test"; then
     echo ""
-    echo "ERROR: The ownership of /home/steam/enshrouded/savegame is not correct and the server will not be able to save..."
+    echo "$(timestamp) ERROR: The ownership of /home/steam/enshrouded/savegame is not correct and the server will not be able to save..."
     echo "the directory that you are mounting into the container needs to be owned by 10000:10000"
     echo "from your container host attempt the following command 'chown -R 10000:10000 /your/enshrouded/folder'"
     echo ""
@@ -60,7 +65,7 @@ fi
 rm "${ENSHROUDED_PATH}/savegame/test"
 
 # Modify server config to match our arguments
-echo "INFO: Updating Enshrouded Server configuration"
+echo "$(timestamp) INFO: Updating Enshrouded Server configuration"
 tmpfile=$(mktemp)
 jq --arg n "$SERVER_NAME" '.name = $n' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
 jq --arg p "$SERVER_PASSWORD" '.password = $p' ${ENSHROUDED_CONFIG} > "$tmpfile" && mv "$tmpfile" $ENSHROUDED_CONFIG
@@ -86,5 +91,28 @@ fi
 ln -sf /proc/1/fd/1 "${ENSHROUDED_PATH}/logs/enshrouded_server.log"
 
 # Launch Enshrouded
-echo "INFO: Starting Enshrouded Dedicated Server"
-${STEAM_PATH}/compatibilitytools.d/GE-Proton${GE_PROTON_VERSION}/proton runinprefix ${ENSHROUDED_PATH}/enshrouded_server.exe
+echo "$(timestamp) INFO: Starting Enshrouded Dedicated Server"
+
+${STEAM_PATH}/compatibilitytools.d/GE-Proton${GE_PROTON_VERSION}/proton run ${ENSHROUDED_PATH}/enshrouded_server.exe &
+
+# Find pid for enshrouded_server.exe
+timeout=0
+while [ $timeout -lt 11 ]; do
+    if ps -e | grep "enshrouded_serv"; then
+        enshrouded_pid=$(ps -e | grep "enshrouded_serv" | awk '{print $1}')
+        break
+    elif [ $timeout -eq 10 ]; then
+        echo "$(timestamp) ERROR: Timed out waiting for enshrouded_server.exe to be running"
+        exit 1
+    fi
+    sleep 6
+    timeout+=1
+    echo "$(timestamp) INFO: Waiting for enshrouded_server.exe to be running"
+done
+
+# I don't love this but I can't use `wait` because it's not a child of our shell
+tail --pid=$enshrouded_pid -f /dev/null
+
+# If we lose our pid, exit container
+echo "$(timestamp) ERROR: He's dead, Jim"
+exit 1
--- makefile
+++ makefile
@@ -8,15 +8,15 @@ PROTON_IMAGE_REF := $(REGISTRY)/$(PROTON_IMAGE)
 # Git commit hash
 HASH := $(shell git rev-parse --short HEAD)
 
-# Buildah/Podman Options
+# Buildah/Podman/Docker Options
 CONTAINER_NAME := enshrouded-test
 VOLUME_NAME := enshrouded-data
 BUILDAH_BUILD_OPTS := --format docker -f ./container/wine/Containerfile
 PODMAN_RUN_OPTS := --name $(CONTAINER_NAME) -d --mount type=volume,source=$(VOLUME_NAME),target=/home/steam/enshrouded/savegame -p 15636:15636/udp -p 15637:15637/udp --env=SERVER_NAME='Enshrouded Containerized Server' --env=SERVER_SLOTS=16 --env=SERVER_PASSWORD='ChangeThisPlease' --env=GAME_PORT=15636 --env=QUERY_PORT=15637
 PROTON_CONTAINER_NAME := enshrouded-proton-test
 PROTON_VOLUME_NAME := enshrouded-proton-data
-PROTON_BUILDAH_BUILD_OPTS := --format docker -f ./container/proton/Containerfile
-PROTON_PODMAN_RUN_OPTS := --name $(PROTON_CONTAINER_NAME) -d --mount type=volume,source=$(PROTON_VOLUME_NAME),target=/home/steam/enshrouded/savegame -p 15636:15636/udp -p 15637:15637/udp --env=SERVER_NAME='Enshrouded Containerized Server' --env=SERVER_SLOTS=16 --env=SERVER_PASSWORD='ChangeThisPlease' --env=GAME_PORT=15636 --env=QUERY_PORT=15637
+PROTON_DOCKER_BUILD_OPTS := -f ./container/proton/Containerfile
+PROTON_DOCKER_RUN_OPTS := --name $(PROTON_CONTAINER_NAME) -d --mount type=volume,source=$(PROTON_VOLUME_NAME),target=/home/steam/enshrouded/savegame -p 15636:15636/udp -p 15637:15637/udp --env=SERVER_NAME='Enshrouded Containerized Server' --env=SERVER_SLOTS=16 --env=SERVER_PASSWORD='ChangeThisPlease' --env=GAME_PORT=15636 --env=QUERY_PORT=15637
 
 # Makefile targets
 .PHONY: build run cleanup build-proton run-proton cleanup-proton
@@ -34,13 +34,13 @@ cleanup:
 	podman volume rm $(VOLUME_NAME)
 
 build-proton:
-	buildah build $(PROTON_BUILDAH_BUILD_OPTS) -t $(PROTON_IMAGE_REF):$(HASH) ./container/proton
+	docker build $(PROTON_DOCKER_BUILD_OPTS) -t $(PROTON_IMAGE_REF):$(HASH) ./container/proton
 
 run-proton:
-	podman volume create $(PROTON_VOLUME_NAME)
-	podman run $(PROTON_PODMAN_RUN_OPTS) $(PROTON_IMAGE_REF):$(HASH)
+	docker volume create $(PROTON_VOLUME_NAME)
+	docker run $(PROTON_DOCKER_RUN_OPTS) $(PROTON_IMAGE_REF):$(HASH)
 
 cleanup-proton:
-	podman rm -f $(PROTON_CONTAINER_NAME)
-	podman rmi -f $(PROTON_IMAGE_REF):$(HASH)
-	podman volume rm $(PROTON_VOLUME_NAME)
+	docker rm -f $(PROTON_CONTAINER_NAME)
+	docker rmi -f $(PROTON_IMAGE_REF):$(HASH)
+	docker volume rm $(PROTON_VOLUME_NAME)
